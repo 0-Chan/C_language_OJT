4750512de57e6c9e2c776d45462905b94a00df9e
cody_kim <kyc4421194@gmail.com>
2021-01-25 15:21:25 +0900

    Linked list CRUD 구현 완료.

--- C_OJT/data_structures/linked_list.c
+++ C_OJT/data_structures/linked_list.c
@@ -1,9 +1,10 @@
+/* Goal : Implementing CRUD using linked list in C
+ * date : 2021. 01 . 25 
+ * author : Young Chan Kim (CogaPlex)
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
-
-#define IS_EQUAL(a, b) (a == b)
-
-void beforeExit(void);
 
 typedef struct Node
 {
@@ -20,10 +21,12 @@ typedef struct
 void initList(LinkedList *list);
 Node* createNode(int item, Node* next);
 void readList(Node* root, int size);
-void insertNode(LinkedList* list, int data, int size);
+void updateNode(LinkedList* list, int size, int data);
+void insertNode(LinkedList* list, int size, int data);
 void deleteNode(LinkedList* list, int size);
 
 int getSize(LinkedList* list);
+void beforeExit(void);
 
 
 int main(void)
@@ -31,22 +34,28 @@ int main(void)
 	LinkedList list;
 	initList(&list);
 
-	insertNode(&list, 1, 1);
-	insertNode(&list, 2, 2);
-	insertNode(&list, 3, 3);
-	insertNode(&list, 4, 2);
+	insertNode(&list, 0, 1);
+	insertNode(&list, 1, 2);
+	insertNode(&list, 2, 3);
+	insertNode(&list, 3, 4);
 
+	readList(&list, 0);
+	readList(&list, 1);
+	readList(&list, 2);
+	readList(&list, 3);
 
-	deleteNode(&list, 1);
+	printf("\n");
 
-	printf("hehe");
+//	updateNode(&list, 2, 1);
 
+	deleteNode(&list, 2);
+	
+	for(int i = 0, n = getSize(&list); i < n; i++)
+	{
+		readList(&list, i);
+	}
 }
 
-// C 생성
-// R 읽기(값)
-// U 수정
-// D 삭제
 
 void initList(LinkedList* list)
 {
@@ -58,7 +67,7 @@ void initList(LinkedList* list)
 Node* createNode(int item, Node* next)
 {
 	Node* n = malloc(sizeof(Node));
-	if (n == NULL)
+	if(n == NULL)
 	{
 		atexit(beforeExit);
 	}
@@ -69,45 +78,72 @@ Node* createNode(int item, Node* next)
 	return n;
 }
 
-
-void readList(Node* root, int size)
+void readList(LinkedList* list, int size)
 {
-	while (root != NULL)			// NULL을 만날 때 까지 ??	
+	int i = 0;
+	Node* tmp = list->head;
+
+	while(tmp != NULL)
 	{
-		root = root->next;
+		if(i == size)
+		{
+			printf("%i\n", tmp->data);
+			return 0;
+		}
 
-	//	if(root-> == size)	// next는 주소
-	//	{
-	//		printf("%i\n", root->data);
-	//	}
+		tmp = tmp->next;
+		i++;
 	}
-} // 미완
+}
 
-void insertNode(LinkedList* list, int data, int size)
+void updateNode(LinkedList* list, int size, int data)
 {
-	if (size < 1 || size >(list->size) + 1)
+	if(size < 0 || size > (list->size) + 1)
+	{
+		printf("Update가 불가능한 위치입니다.");
+		atexit(beforeExit);
+	}
+
+	int i = 0;
+	Node* tmp = list->head;
+
+	while(tmp != NULL)
+	{
+		if(i == size)
+		{
+			tmp->data = data;
+			return 0;
+		}
+
+		tmp = tmp->next;
+		i++;
+	}
+}
+
+void insertNode(LinkedList* list, int size, int data)
+{
+	if(size < 0 || size > (list->size) + 1)
 	{
 		printf("Insert가 불가능한 위치입니다.");
-		exit(1);
+		atexit(beforeExit);
 	}
 
 	Node* n = createNode(data, NULL);
 
-	if(size == 1)
+	if(size == 0)
 	{
 		n->next = list->head;
-
 		list->head = n;
 	}
 	else
 	{
-		Node* tmp = list->head;		// 한 바퀴를 더감
-		for(int i = 0; i < size - 2; i++)	// zero base로 갑시다. 1 base 말고.
+		Node* tmp = list->head;
+		for(int i = 0; i < size - 1; i++)
 		{
 			tmp = tmp->next;
 		}
 
-		n->next = tmp->next;		// 먼가 꼬임
+		n->next = tmp->next;
 		tmp->next = n;
 	}
 	(list->size)++;
@@ -115,15 +151,17 @@ void insertNode(LinkedList* list, int data, int size)
 
 void deleteNode(LinkedList* list, int size)
 {
-	if(size < 1 || size >(list->size) + 1)
+	if(size < 0 || size > (list->size) + 1)
 	{
 		printf("Delete이 불가능한 위치입니다.");
-		exit(1);
+		atexit(beforeExit);
 	}
 	
+	int i = 0;
 	Node* tmp = list->head;
+	Node* n = NULL;
 
-	if(size == 1)
+	if(size == 0)
 	{
 		list->head = tmp->next;
 
@@ -132,69 +170,40 @@ void deleteNode(LinkedList* list, int size)
 	}
 	else
 	{
-		for(int i = 0; i < size - 1; i++)
+		while(tmp != NULL)
 		{
-			list->head->next;
-			printf("%p\n", list->head->next);
+			if(i == size - 1)
+			{
+				n = tmp;
+				tmp = tmp->next;
+
+				n->next = tmp->next;
+
+				free(tmp);
+				(list->size)--;
+				return 0;
+			}
+			tmp = tmp->next;
+			i++;
 		}
 	}
 }
 
-// update , creat 함께
-// delete와 read 함께
-
 int getSize(LinkedList* list)
 {
-	//while ( != NULL)
+	Node* tmp = list->head;
+	int size = 0;
+
+	while(tmp != NULL)
 	{
-		
+		tmp = tmp->next;
+		size++;
 	}
 
-	// list->next가 null을 만날 때 까지
-	// 반복하여 size에 넣는다.
-	// 그걸 리턴
-
-	return 1;
-} // 미완
-
+	return size;
+}
 
 void beforeExit(void)
 {
 	printf("exit() is called\n");
 }
-
-
-/*
-* 
-	Node* n = malloc(sizeof(Node));
-	if (n == NULL)
-	{
-		return 1;
-	}
-
-	n->data = item;
-	n->next = next;
-
-	
-* 
-* 
-* 
-Node * convertToList(int* arr, int arrLength)
-{
-	Node* root = NULL;
-	for (int i = arrLength - 1; i >= 0; i--)
-	{
-		insertNode(&root, arr[i]);
-	}
-	return root;
-}
-
-void display(Node* root)
-{
-	while (root != NULL)
-	{
-		printf("%i\n", root->data);
-		root = root->next;
-	}
-}
-*/
