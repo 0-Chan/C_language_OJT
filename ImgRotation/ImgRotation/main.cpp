1249d6d1e39a2290acf9db22748f6286b63cd3d3
YoungChan Kim <kyc4421194@gmail.com>
2021-02-05 18:12:49 +0900

    updates 90 degree rotation feature

--- ImgRotation/ImgRotation/main.cpp
+++ ImgRotation/ImgRotation/main.cpp
@@ -18,7 +18,7 @@ typedef struct
     int y;
 } Coords;
 
-Coords rotationMatrix(int degree, Coords input);
+Coords rotationMatrix(double degree, Coords input, Coords base);
 
 int main(int argc, char* argv[])
  {
@@ -33,47 +33,36 @@ int main(int argc, char* argv[])
     double val = atof(degree);
     double radian = (PI / 180) * val;
 
+    Coords inputXY, resXY, base;
+    resXY.x = 0;
+    resXY.y = 0;
 
-    Coords inputXY, resXY;
-    uchar inPixel = 0;
-    uchar resPixel = 0;
 	Mat img = imread(imgName, IMREAD_GRAYSCALE);
-//    Mat rotImg = imread(imgName, IMREAD_GRAYSCALE);
-
-    Mat rotImg = Mat::zeros(500, 500, img.type());
-
     int rows = img.rows;
-    int cols = img.cols;   
+    int cols = img.cols;
+    int gap = 2 * (cols - rows);
+    Mat rotImg = Mat::zeros(cols, rows + gap, img.type());
 
-    
+    imshow("original", img);
+
+    base.x = cols;
+    base.y = rows;
+
     for (int i = 0; i < rows; i++)
     {
         for (int j = 0; j < cols; j++)
         {
             inputXY.x = j;
             inputXY.y = i;
-            inPixel = img.at<uchar>(i, j);
 
-            resXY = rotationMatrix(radian, inputXY);
-            cout << " res : " << resPixel << "in : " << inPixel << endl;
-            resPixel = rotImg.at<uchar>(resXY.y, resXY.x);
+            resXY = rotationMatrix(radian, inputXY, base);
 
-            resPixel = inPixel;
-            
-            cout << " res : " << resPixel << "in : " << inPixel << endl;
+            rotImg.at<uchar>((int)resXY.y, (int)resXY.x) = img.at<uchar>(i, j);
         }
     }
 
-    img.at<uchar>();
-
     cout << "Rows : " << rows << endl;
     cout << "Cols : " << cols << endl;
-
-    imshow("original", img);
-//    cout << img << endl;
-
-    img.at<uchar>(100, 100);
-
 
 	imshow("result", rotImg);
 	waitKey(0);
@@ -82,40 +71,19 @@ int main(int argc, char* argv[])
 }
 
 
-Coords rotationMatrix(int radian, Coords input)
+Coords rotationMatrix(double radian, Coords input, Coords base)
 {
     Coords res;
+    res.x = 0; 
+    res.y = 0;
 
-    res.x = cos(radian) * input.x + (sin(radian) * -1) * input.y;
-    res.y = sin(radian) * input.x + cos(radian) * input.y;
+    res.x = (cos(radian) * (input.x - base.x)) - (sin(radian) * input.y) + base.x;
+    res.y = (sin(radian) * (input.x - base.y)) + (cos(radian) * input.y) + base.y;
 
-    if (res.x < 1 || res.y < 1)
+    if (res.x < 0 || res.y < 0 )
     {
         res.x = 0;
         res.y = 0;
     }
-    
     return res;
 }
-
-/* 수도 코드
-1) img.at<uchar>(i, j);
-
-2) 반복문 i 는 행row를
-3) 반복문 j 는 열col을 받는다.
-
-행렬 값을 받고
-
-4) 행렬을 특정 degree 만큼 돌린다.
-
-5) 어떻게? 회전변환행렬을 이용해서.
-cos 세타  -sin 세타
-sin 세타   cos 세타
-
-6) 결과 값을 미리 변환해둔 frame 에다 갖다 넣는다.
-
-7) 출력한다.
-
-Rows : 121
-Cols : 288
-*/
